eed740cd0a4c7e6ec1b6228fdc6a709ef664f350
Prathampoojit Bandi <prathampoojit@gmail.com>
2026-04-08 17:50:48 +0000

    previous ver : manual reversing. current ver : applied in-built array reverse method

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,9 +1,5 @@
 const reverseString = function(string) {
-    let arr = string.split("");
-    for(let i = 0; i < arr.length / 2; i++){
-        [arr[i], arr[arr.length - i - 1]] = [arr[arr.length - i - 1],arr[i]];
-    }
-    return arr.join("");
+    return string.split("").reverse().join("");
 
 };
 
