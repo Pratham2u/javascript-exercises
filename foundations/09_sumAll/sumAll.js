226be605d11415977b617798f7cff308115d6537
Prathampoojit Bandi <prathampoojit@gmail.com>
2026-04-09 15:33:04 +0000

    Done!

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function( a , b) {
+    if((!Number.isInteger(a)) || (!Number.isInteger(b))){
+        return "ERROR";
+    }
+    if(a < 0 || b < 0) return "ERROR"
 
+    let start = Math.min(a,b);
+
+    for(let i = start+1; i<= Math.max(a,b); i++){
+        start += i;
+    }
+    return start;
 };
 
 // Do not edit below this line
