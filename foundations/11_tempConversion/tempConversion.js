03b01066b4b5de646e64168197593409e70a8a6c
Prathampoojit Bandi <prathampoojit@gmail.com>
2026-04-09 16:37:36 +0000

    Done!

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let ans = 5 * (fahrenheit - 32) / 9;
+  return (Math.round(ans * 10)) / 10;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let ans = (celsius * 9 / 5) + 32;
+  return (Math.round(ans * 10)) / 10;
 };
 
 // Do not edit below this line
